487101741aace7593536d4b366cde616f026a87c
Anderware <81265884+Anderware@users.noreply.github.com>
2021-04-13 20:55:49 +0200

    Add files via upload
    Added cancel

--- modPrompt/modprompt.js
+++ modPrompt/modprompt.js
@@ -5,21 +5,31 @@
         content: '<label style="padding-right:12px;" for="modPromptValue" >' + rollname + '</label><input style="width:64px" id="modPromptValue" name="modPromptValue" value="0" type="number"><hr>',
         buttons: {
           ok: {
+            icon:'<i class ="fas fa-check"></i>',
             label: "Ok",            
             callback: (html) => {
               let value = html.find('input[name=modPromptValue]').val();
               resolve(value);
             }
+          },
+          cancel: { 
+            icon:'<i class ="fas fa-times"></i>',
+            label: "Cancel",            
+            callback: (html) => {              
+              resolve(-999999);
+            }
           }
         },
         default: "ok",
-        close:  html => {              
-              resolve(0);
-              }   
+        close:  html => {                            
+        }   
       }).render(true);             
     });    
     console.log(rollname);
-    let modpromptvalue=await dialog;
+    let modpromptvalue=await dialog;  
+    if (modpromptvalue==-999999){
+      throw new Error("User aborted roll by clicking Cancel button.");
+    }
     return modpromptvalue;
   }
    
