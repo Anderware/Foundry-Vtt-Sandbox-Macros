facf646214f945027023529911c961b7d52d3aa9
Anderware <81265884+Anderware@users.noreply.github.com>
2021-10-26 23:56:43 +0200

    Add files via upload

--- Macros/Various/ExecuteActorRollForSelectedToken.js
+++ Macros/Various/ExecuteActorRollForSelectedToken.js
@@ -1,4 +1,14 @@
-ExecuteActorRollForSelectedToken('BodyRoll');
+// **************************************************************** 
+// Macro:        ExecuteActorRollForSelectedToken                                                    
+// Description:  Rolle a property based on its name                   
+// ================================================================ 
+// Date       Version  Author               Description             
+// ---------- -------- -------------------- ----------------------- 
+// 2021-05-05 1.0.0    Ramses800            Macro created.
+// 2021-10-26 1.1.0    Ramses800            Added target         
+// **************************************************************** 
+
+ExecuteActorRollForSelectedToken('ATTACK');
 
 async function  ExecuteActorRollForSelectedToken(sPropertyKey){
     // get selected token
@@ -7,7 +17,7 @@ async function  ExecuteActorRollForSelectedToken(sPropertyKey){
     if(selected.length>0){
       if(selected.length==1){
         let token = selected[0];  
-        // check if token has citem   
+        // check if token has attribute 
         let actor=token.actor;
         let actorattributes = actor.data.data.attributes;        
         if (actorattributes!=null){ 
@@ -23,8 +33,13 @@ async function  ExecuteActorRollForSelectedToken(sPropertyKey){
                   // execute it                    
                   let rollid = [];             
                   let rollname = property.data.data.rollname;         
-                  rollid.push(property.data.data.rollid);           
-                  actor.rollSheetDice(rollexp,rollname,rollid,actorattributes,null); 
+                  rollid.push(property.data.data.rollid);  
+                  let citemattributes=null;
+                  let number=1;
+                  let targets = game.user.targets.ids; // get list of currently selected targets by the current user
+                  let target = canvas.tokens.placeables.find(y=>y.id==targets[0]); // this will selected the first targeted token
+                  let rollcitemID = null       
+                  actor.rollSheetDice(rollexp,rollname,rollid,actorattributes,citemattributes,number,target,rollcitemID); 
                 }
                 else{
                   ui.notifications.warn('The roll expression for property ' + sPropertyKey + ' is empty');
