337435ff48326936816601bfb458db0daaf462ad
Anderware <81265884+Anderware@users.noreply.github.com>
2022-02-25 23:56:19 +0100

    Add files via upload

---
+++ Macros/Various/ExecutecItemRollForSelectedToken_1.3.js
@@ -0,0 +1,87 @@
+// **************************************************************** 
+// Macro:        ExecutecItemRollForSelectedToken                                                    
+// Description:  Rolls a rollable citem property based on its name(key) 
+// Compability:  Tested with 
+//               Foundry 9.249 - Sandbox 0.12.2                  
+// ================================================================ 
+// Date       Version  Author               Description             
+// ---------- -------- -------------------- ----------------------- 
+// 2021-05-05 1.0.0    Ramses800            Macro created.
+// 2021-10-26 1.1.0    Ramses800            Added target
+// 2021-11-04 1.2.0    Ramses800            Added roll dialogs support
+// 2022-02-25 1.3.0    Ramses800            Updated for sb 0.12           
+// **************************************************************** 
+
+// example
+
+ExecutecItemRollForSelectedToken('Sword','LBL_WEAPON_ATTACK_ROLL');
+
+async function ExecutecItemRollForSelectedToken(scItemName,sPropertyKey){
+  // get selected token
+  let selected = canvas.tokens.controlled; 
+  if (selected!=null){
+    if(selected.length>0){
+      if(selected.length==1){
+        let token = selected[0];  
+        // check if token has citem   
+        let actor=token.actor;
+        let citem = actor.data.data.citems.find(y=>y.name == scItemName);
+        if (citem!=null){ 
+          // check if it has the property
+          let property = game.items.find(y=>y.type=="property" && y.data.data.attKey==sPropertyKey);
+          if (property!=null){
+            // check if the property has a roll
+            let rollexp = property.data.data.rollexp;
+            if (rollexp!=null){
+              if (rollexp.length>0){
+                // execute it 
+                let citemattributes = citem.attributes;              
+                let rollname = property.data.data.rollname; 
+                let hasDialog = property.data.data.hasdialog;                
+                let useData = null;
+                let rollid = [];   
+                rollid.push(property.data.data.rollid);   
+                let actorattributes = actor.data.data.attributes;
+                let number=1;
+                let targets = game.user.targets.ids; // get list of currently selected targets by the current user
+                let target = canvas.tokens.placeables.find(y=>y.id==targets[0]); // this will selected the first targeted token
+                let rollcitemID = citem.id;    
+                //debugger;  
+                if (hasDialog) {                    
+                    let dialogID = property.data.data.dialogID;
+                    let dialogName = property.data.data.dialogName;
+                    actor._sheet.generateRollDialog(dialogID,dialogName, rollexp, rollname, rollid, actorattributes, citemattributes, number, rollcitemID, targets, useData);
+                }
+                else {
+                  actor.rollSheetDice(rollexp,rollname,rollid,actorattributes,citemattributes,number,target,rollcitemID);
+                } 
+              }
+              else{
+                ui.notifications.warn('The roll expression for property ' + sPropertyKey + ' is empty');
+              }      
+            }
+            else{
+              ui.notifications.warn('The property ' + sPropertyKey + ' does not have a rollexpression');
+            } 
+          }
+          else{
+            ui.notifications.warn('The cItem '+ scItemName + ' does not have the property ' + sPropertyKey);
+          }
+        }
+        else{
+          // 
+          ui.notifications.warn('The selected token(' + token.data.name + ') does not have the cItem ' + scItemName );
+        }
+      }
+      else{
+        ui.notifications.warn('More than one token is selected');
+      }
+    }
+    else{
+      ui.notifications.warn('No token selected');
+    }     
+  }
+  else{
+    ui.notifications.error('Error getting selected tokens');
+  }
+}
